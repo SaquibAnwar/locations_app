0f3a5ce906255e438130e8627823c3677507c32b
SaquibAnwar <saquibedu@gmail.com>
2020-04-13 21:44:16 +0530

    adding input validations

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -13,20 +13,20 @@ const DUMMY_PLACES = [
     address: "Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001",
     location: {
       lat: 27.1751448,
-      lng: 78.0399535
+      lng: 78.0399535,
     },
     creator: "u1",
   },
   {
     id: "p2",
-    title: "Taj Mahal",
+    title: "Empire State Building",
     description: " One of the most famous monuments in the world!",
     imageUrl:
-      "https://images.unsplash.com/photo-1564507592333-c60657eea523?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80",
-    address: "Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001",
+      "https://images.unsplash.com/photo-1502104034360-73176bb1e92e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    address: "20 W 34th St, New York, NY 10001, United States",
     location: {
-      lat: 27.1751448,
-      lng: 78.0399535
+      lat: 40.7484405,
+      lng: -73.9878531,
     },
     creator: "u2",
   },
@@ -34,7 +34,7 @@ const DUMMY_PLACES = [
 
 const UserPlaces = () => {
   const userId = useParams().userId;
-  const laodedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+  const laodedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
   return <PlaceList items={laodedPlaces} />;
 };
 
